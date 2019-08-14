f9926409b16b9fb9f0e49a59c3704581ae453842
prince-ph0en1x <aritra44sarkar@yahoo.co.in>
2019-08-14 15:47:55 +0200

    get state test

--- QAOA_DeNovoAsb/code_000.py
+++ QAOA_DeNovoAsb/code_000.py
@@ -3,24 +3,38 @@
 from qxelarator import qxelarator
 import numpy as np
 
-p_name = "test_output/multi_run.qasm"
-n_qubits = 3
-shots = 400
+def test_01():
+	p_name = "test_output/multi_run.qasm"
+	n_qubits = 3
+	shots = 400
 
-qx = qxelarator.QX()
-for p_loop in range(0,40):
-	# qx = qxelarator.QX()	# Inside loop cause problems
-	qx.set(p_name)
-	p = np.zeros(2**n_qubits)
-	c = np.zeros(n_qubits,dtype=bool)
-	for i in range(shots):
-		print(p_loop,i)
-		# qx.execute(1)
-		qx.execute() # Both works for p_loop 40 x shots 400
-		for i in range(n_qubits):
-			c[i] = qx.get_measurement_outcome(i)
-		idx = sum(v<<i for i, v in enumerate(c[::-1]))    
-		p[idx] += 1/shots
+	qx = qxelarator.QX()
+	for p_loop in range(0,40):
+		# qx = qxelarator.QX()	# Inside loop cause problems
+		qx.set(p_name)
+		p = np.zeros(2**n_qubits)
+		c = np.zeros(n_qubits,dtype=bool)
+		for i in range(shots):
+			print(p_loop,i)
+			# qx.execute(1)
+			qx.execute() # Both works for p_loop 40 x shots 400
+			for i in range(n_qubits):
+				c[i] = qx.get_measurement_outcome(i)
+			idx = sum(v<<i for i, v in enumerate(c[::-1]))    
+			p[idx] += 1/shots
 
-print(p)
+	print(p)
 
+# test_01()
+
+def test_02():
+	p_name = "test_output/multi_run.qasm"
+	qx = qxelarator.QX()
+	for p_loop in range(0,2):
+		qx.set(p_name)
+		# qx.execute(1) # Doesn't work with get_state()
+		qx.execute() 
+		isv = qx.get_state()
+		print(isv)
+
+test_02()
--- QAOA_DeNovoAsb/test_output/multi_run.qasm
+++ QAOA_DeNovoAsb/test_output/multi_run.qasm
@@ -3,6 +3,7 @@ qubits 3
 h q[0]
 h q[1]
 h q[2]
-measure q[0]
-measure q[1]
-measure q[2]
+#display
+#measure q[0]
+#measure q[1]
+#measure q[2]
